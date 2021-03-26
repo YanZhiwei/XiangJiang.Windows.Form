e2599c01f273f9b4576b937f837628b3e418bae3
言志伟 <Yan.Zhiwei@hotmail.com>
2021-03-26 12:27:39 +0800

    添加项目文件。

---
+++ XiangJiang.Windows.Form/Common/TabPageHelper.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows.Forms;
+
+namespace XiangJiang.Windows.Form.Common
+{
+    /// <summary>
+    ///     TabPage 帮助类
+    /// </summary>
+    /// 时间：2016-01-12 17:28
+    /// 备注：
+    public static class TabPageHelper
+    {
+        #region Methods
+
+        /// <summary>
+        ///     设置Enabled属性
+        /// </summary>
+        /// <param name="tabpage">TabPage</param>
+        /// <param name="enable">if set to <c>true</c> [enable].</param>
+        /// 时间：2016-01-12 17:29
+        /// 备注：
+        public static void SetEnabled(this TabPage tabpage, bool enable)
+        {
+            ((Control) tabpage).Enabled = false;
+        }
+
+        #endregion Methods
+    }
+}
